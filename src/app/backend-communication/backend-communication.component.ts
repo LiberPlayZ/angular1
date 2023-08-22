caad7faef72141432ced257202b454d0e2f0ecd5
Daniel Liberman <dandosh5090@gmail.com>
2023-08-22 20:30:36 +0300

    add the label for  correlation and prediction that change color by data

--- src/app/backend-communication/backend-communication.component.ts
+++ src/app/backend-communication/backend-communication.component.ts
@@ -9,47 +9,64 @@ import { ImageModel } from './image.mode';
   templateUrl: './backend-communication.component.html',
   styleUrls: ['./backend-communication.component.css']
 })
-export class BackendCommunicationComponent   {
-  showImageFlag = false
+export class BackendCommunicationComponent {
+ 
 
- //send data to python backend
- constructor(private dataService: DataService,private router:Router) {}
+  //send data to python backend
+  constructor(private dataService: DataService, private router: Router) { }
 
-  public IMAGE:string;
-  inputValue:number = 0;
-  showImage(): void {
-    this.dataService.recivegraphdata().subscribe(
-      (response: any) => {
-        console.log(response.graph_data);
-        if(this.showImageFlag==false){
-        this.showImageFlag=!this.showImageFlag
+  IMAGE: string;
+  inputValue: number = 0;
+  received_correlation_coefficient: number
+  receivedText: string
+  labelVisible = false
+
+  // showImage(): void {
+  //   this.dataService.recivegraphdata().subscribe(
+  //     (response: any) => {
+  //       console.log(response.graph_data);
+  //       if(this.labelVisible==false){
+  //       this.labelVisible=!this.labelVisible
+  //       }
+  //       this.IMAGE="data:image/png;base64,"+response.graph_data;
+  //     //  this.router.navigate(['/view-image'], { queryParams: { imageBase64: response.graph_data } });
+  //     },
+  //     (error: any) => {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   );
+  // }
+  sendGraphIdToBackEnd() {
+    if (this.inputValue) {
+
+      this.dataService.sendGraphId(this.inputValue).then((response: ImageModel) => {
+        if (this.labelVisible == false) {
+          this.labelVisible = !this.labelVisible
+        } 
+        if(response.correlation_coefficient==0){
+          this.receivedText="there is no connection ";
         }
-        this.IMAGE="data:image/png;base64,"+response.graph_data;
-      //  this.router.navigate(['/view-image'], { queryParams: { imageBase64: response.graph_data } });
-      },
-      (error: any) => {
-        console.error('Error fetching data:', error);
-      }
-    );
+        else{
+        this.receivedText=`prediction percentage is ${response.prediction_percentage} %`;
+        }
+        this.received_correlation_coefficient=response.correlation_coefficient;
+        //  console.log(" prediction percentage is" +response.correlation_coefficient)
+        this.IMAGE = "data:image/png;base64," + response.graph_data;
+      })
+
+    }
   }
-  sendGraphIdToBackEnd(){
-        if (this.inputValue){
-         
-          this.dataService.sendGraphId(this.inputValue).then((response:ImageModel)=>
-          {
-            console.log(response.correlation_coefficient)
-          })
-          // .subscribe(
-          //   response=>{
-          //     console.log('response',response);
-          //     // this.showImage()
-          //   },
-          //   error=>{
-          //     console.log('eror',error)
-          //   }
-
-          // );
-        }
+  getColorClass(number:number):string{
+     if (number==0)
+        return 'no_connection' ;
+    if (Math.abs(number) <= 0.3)
+        return 'low_connection';
+    else if (0.3 < Math.abs(number)&& Math.abs(number)<= 0.7)
+        return 'medium_connection' ;
+    else if ( 0.7 <Math.abs(number)&& Math.abs(number)< 1)
+        return 'strong_connection' ;
+    else
+        return 'perfect_connection' ;
   }
 
   // imageData: string;
@@ -61,13 +78,13 @@ export class BackendCommunicationComponent   {
   //   this.imageData = this.route.snapshot.queryParamMap.get('imageBase64');
   // }
 
- 
-  
+
+
   // getSafeImageURL(): SafeResourceUrl {
   //   const imageUrl = 'data:image/png;base64,' + this.imageData;
   //   return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
   // }
-   // ngOnInit(): void {
+  // ngOnInit(): void {
   //       this.dataService.recivegraphdata().subscribe(
   //         (response: any) => {
   //           // response.graph_data=new TextEncoder().encode(response.graph_data)
@@ -76,18 +93,18 @@ export class BackendCommunicationComponent   {
   //           // this.imageBase64=response.graph_data
   //           this.router.navigate(['/view-image', { imageBase64: response.graph_data }]);
 
-    
+
   //         },
   //         (error: any) => {
   //           console.error('Error fetching data:', error);
   //         }
   //       );
-  
-  
-        // getSafeImageURL(): SafeResourceUrl {
-        //   const imageUrl = 'data:image/png;base64,' + this.imageBase64;
-        //   return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
-        // }
+
+
+  // getSafeImageURL(): SafeResourceUrl {
+  //   const imageUrl = 'data:image/png;base64,' + this.imageBase64;
+  //   return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
+  // }
 
 
 
@@ -116,24 +133,24 @@ export class BackendCommunicationComponent   {
   //   const imageUrl = 'data:image/png;base64,' + this.imageBase64;
   //   return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
   // }
-  
 
-//   recivegraphdata(): void {
-//     this.dataService.recivegraphdata().subscribe(
-//       (response: any) => {
-//         // response.graph_data=new TextEncoder().encode(response.graph_data)
-//         // const recievedata=response
-//         console.log('Received data:', response);
-//         this.imageBase64 = response.image;
-//         const imageUrl = 'data:image/png;base64,' + this.imageBase64;
-//         return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
-//         // const decodedImage = atob(response.graph_data);
-//         // const blob = new Blob([decodedImage], { type: 'image/png' });
-//         // this.safeImageUrl = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
 
-//       },
-//       (error: any) => {
-//         console.error('Error fetching data:', error);
-//       }
-//     );
+  //   recivegraphdata(): void {
+  //     this.dataService.recivegraphdata().subscribe(
+  //       (response: any) => {
+  //         // response.graph_data=new TextEncoder().encode(response.graph_data)
+  //         // const recievedata=response
+  //         console.log('Received data:', response);
+  //         this.imageBase64 = response.image;
+  //         const imageUrl = 'data:image/png;base64,' + this.imageBase64;
+  //         return this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
+  //         // const decodedImage = atob(response.graph_data);
+  //         // const blob = new Blob([decodedImage], { type: 'image/png' });
+  //         // this.safeImageUrl = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+
+  //       },
+  //       (error: any) => {
+  //         console.error('Error fetching data:', error);
+  //       }
+  //     );
 }
--- src/app/backend-communication/image.mode.ts
+++ src/app/backend-communication/image.mode.ts
@@ -1,5 +1,5 @@
-export interface ImageModel{
-    graph_data:string;
-    prediction_percentage:number;
-    correlation_coefficient:number;
+export interface ImageModel {
+    graph_data: string;
+    prediction_percentage: number;
+    correlation_coefficient: number;
 }
